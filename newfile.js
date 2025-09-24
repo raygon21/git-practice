401ce538c9655bed8b68280c73d1e92be026cfbb
raygon21 <gonzalez.raymond67@gmail.com>
2025-09-24 11:35:21 -0700

    Update newfile.js

---
+++ newfile.js
@@ -0,0 +1,10 @@
+// This is a simple JavaScript file named 'script.js'
+
+// 1. You can store information in a variable
+let message = "Hello, World!";
+
+// 2. You can perform an action, like showing a pop-up alert
+alert(message);
+
+// 3. You can write content directly to the web page
+document.getElementById("greeting").innerHTML = "This message is from the JavaScript file.";
